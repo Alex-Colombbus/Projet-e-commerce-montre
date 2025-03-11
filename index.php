6f906b034a423cab08fb2de23ba26120a63e34a9
Facewall <lmdm@gmx.fr>
2025-03-11 16:57:11 +0100

    MAJ Style et index et base images

--- index.php
+++ index.php
@@ -4,9 +4,11 @@ require_once("./includes/header.inc.php");
 ?>
 
 <main>
-      <div id="hero">
-
-            <h2>Choisissez votre partenaire de tous moments :</h2>
+      <div id="hero" class="container-fluid position-relative">
+            <div class="positon-absolute bottom-0 start-50  mb-3 ">
+            <h2 >Choisissez votre partenaire de tous moments :</h2>
+            </div>
+          
             <h2>Votre montre Maison du Temps</h2>
           
       </div>
