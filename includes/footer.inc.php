afa158cdd40a7f688e151c3905377ee7a16331b8
Facewall <faycal.bourgeois@gmail.com>
2025-03-11 14:01:47 +0100

    MAJ footer

--- includes/footer.inc.php
+++ includes/footer.inc.php
@@ -1,3 +1,49 @@
+
+<footer>
+<div class="container-fluid nav-color text-white mt-5 p-4">
+      <div class="row text-center">
+            <div class="col-2">
+                  <ul class="navbar-nav">
+                        <li class="nav-item">
+                              <a class="nav-link px-3  text-start" href="#">Marques populaires</a>
+                        </li>
+                        <li class="nav-item">
+                              <a class="nav-link px-3  text-start" href="#">Hommes</a>
+                        </li>
+                        <li class="nav-item">
+                              <a class="nav-link px-3  text-start" href="#">Femmes</a>
+                        </li>
+                        <li class="nav-item">
+                              <a class="nav-link px-3  text-start" href="#">Garçons</a>
+                        </li>
+                        <li class="nav-item">
+                              <a class="nav-link px-3  text-start" href="#">Filles</a>
+                        </li>
+                  </ul>
+            </div>
+            <div class="col-2 ">
+                  <ul class="navbar-nav">
+                  <li class="nav-item">
+                        <a class="nav-link px-3 text-start" href="#">Pages populaires</a>
+                  </li>
+                  <li class="nav-item">
+                        <a class="nav-link px-3  text-start" href="#">Montres Hommes</a>
+                  </li>
+                  <li class="nav-item">
+                        <a class="nav-link px-3  text-start" href="#">Montres Femmes</a>
+                  </li>
+                  </ul>
+            </div>
+            <div class="col-4 align-content-center">
+                  <a class="navbar-brand" href="<?= RACINE_SITE ?>index.php" >
+                  <img src="<?= RACINE_SITE ?>assets/img/logoMaisonduTemps.png" alt="Maison du Temps" width="200" height="auto"></a>
+            </div>
+            <div class="col-2 ">login</div>
+            <div class="col-2 ">cart</div>    
+      </div>
+</div>
+</footer>
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
       integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
       crossorigin="anonymous"></script>
