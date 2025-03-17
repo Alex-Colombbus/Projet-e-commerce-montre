0b300f0de6b337e75c0451e669938fc274864a90
Alex :) <alexandre.cavet@colombbus.org>
2025-03-17 16:50:05 +0100

    dz

--- products/product.php
+++ products/product.php
@@ -14,29 +14,29 @@ require_once("../includes/header.inc.php");
                         <div>
                               <h4>Montre Timex Expedition North TW2V54000 Expedition North - Automatic</h4>
                         </div>
-                        <hr>
+                        <hr class="ligneOr">
                         <div class="row ">
                               <h4 class="col-6">Prix : 349€</h4>
-                              <p class="text-success col-6 text-end">en Stock</p>
+                              <p class="stock col-6 text-end fs-3 fw-bold">en stock</p>
                               <div class="d-flex justify-content-center">
-                                    <button class="bouttonCommander">Commander</button>
+                                    <button class="btn rounded-pill bouttonCommander fw-bold px-4">Commander</button>
                               </div>
                         </div>
-                        <hr>
+                        <hr class="ligneOr">
                         <div>
                               <h4>Fonctions :</h4>
                         </div>
                         <div>
                               <div class="row">
                                     <p class="col-6 fw-bold"><span>Chronomètre</span></p>
-                                    <p class="col-6 text-end">Chronomètre /Chronographe</p>
+                                    <p class="col-6 text-end">Chronomètre / Chronographe</p>
                               </div>
-                              <hr>
+                              <hr class="ligneOr">
                               <div class="row">
                                     <p class="col-6 fw-bold"><span>Calendrier</span></p>
                                     <p class="col-6 text-end">Date / Fenêtre</p>
                               </div>
-                              <hr>
+                              <hr class="ligneOr">
 
                         </div>
 
@@ -44,96 +44,148 @@ require_once("../includes/header.inc.php");
             </div>
       </section>
       <section class="ficheProduit">
-            <div class="backgroundFicheProduit">
-                  <h4>Fiche produit</h4>
+            <div class="backgroundFicheProduit d-flex align-items-center text-white">
+                  <div class="container">
+                        <h4>Fiche produit</h4>
+                  </div>
             </div>
             <div class="row">
-                  <div class="container col-6">
-                        <h5>Informations Générales</h5>
-                        <div class="">
-                              <p class="fw-bold fs-6">EAN</p>
-                              <p class="text-end fs-6">0194366241051</p>
+                  <div class="container col-4">
+                        <h5 class="pt-3 pb-3 fw-bold">Informations Générales</h5>
+                        <div class="row">
+                              <p class="fw-bold fs-6 col-6">EAN</p>
+                              <p class="text-end fs-6 col-6">0194366241051</p>
                         </div>
-                        <div class="">
-                              <p class=" fw-bold fs-6">Marque</p>
-                              <p class="text-end fs-6">Timex</p>
+                        <div class="row">
+                              <p class="fw-bold fs-6 col-6">Marque</p>
+                              <p class="text-end fs-6 col-6">Timex</p>
                         </div>
-                        <div class="">
-                              <p class=" fw-bold fs-6">Style</p>
-                              <p class="text-end fs-6">Expedition North</p>
+                        <div class="row">
+                              <p class=" fw-bold fs-6 col-6">Style</p>
+                              <p class="text-end fs-6 col-6">Expedition North</p>
                         </div>
-                        <div class="">
-                              <p class=" fw-bold fs-6">Nom du modèle</p>
-                              <p class="text-end fs-6">Expedition North - Automatic</p>
+                        <div class="row">
+                              <p class=" fw-bold fs-6 col-6">Nom du modèle</p>
+                              <p class="text-end fs-6 col-6">Expedition North - Automatic</p>
                         </div>
-                        <div class="">
-                              <p class=" fw-bold fs-6">Année</p>
-                              <p class="text-end fs-6">2022 Automne / Hiver</p>
+                        <div class="row">
+                              <p class=" fw-bold fs-6 col-6">Année</p>
+                              <p class="text-end fs-6 col-6">2022 Automne / Hiver</p>
                         </div>
-                        <div class="">
-                              <p class=" fw-bold fs-6">Type d'affichage</p>
-                              <p class="text-end fs-6">Analogique</p>
+                        <div class="row">
+                              <p class=" fw-bold fs-6 col-6">Type d'affichage</p>
+                              <p class="text-end fs-6 col-6">Analogique</p>
                         </div>
-                        <div class="">
-                              <p class=" fw-bold fs-6">Étanchéité</p>
-                              <p class="text-end fs-6">20 Bar (plongée)</p>
+                        <div class="row">
+                              <p class=" fw-bold fs-6 col-6">Étanchéité</p>
+                              <p class="text-end fs-6 col-6">20 Bar (plongée)</p>
                         </div>
-                        <div class="">
-                              <p class=" fw-bold fs-6">Couleur du cadran</p>
-                              <p class="text-end fs-6">Noir</p>
+                        <div class="row">
+                              <p class=" fw-bold fs-6 col-6">Couleur du cadran</p>
+                              <p class="text-end fs-6 col-6">Noir</p>
                         </div>
-                        <hr>
+                        <hr class="ligneOr">
                         <div>
-                              <h4>Informations mouvement</h4>
-                              <div class="">
-                                    <p class=" fw-bold fs-6">Type d'affichage</p>
-                                    <p class="text-end fs-6">Analogique</p>
+                              <h5 class="pt-3 pb-3 fw-bold">Informations boîtier</h5>
+                              <h5>Informations mouvement</h5>
+                              <div class="row">
+                                    <p class=" fw-bold fs-6 col-6">Type d'affichage</p>
+                                    <p class="text-end fs-6 col-6">Analogique</p>
                               </div>
-                              <div class="">
-                                    <p class=" fw-bold fs-6">Mécanisme</p>
-                                    <p class="text-end fs-6">Mécanique automatique</p>
+                              <div class="row">
+                                    <p class=" fw-bold fs-6 col-6">Mécanisme</p>
+                                    <p class="text-end fs-6 col-6">Mécanique automatique</p>
                               </div>
                         </div>
                   </div>
-                  <div class="container col-6">
-                        <h5>Informations boîtier</h5>
-                        <div class="">
-                              <p class=" fw-bold fs-6">Diamètre</p>
-                              <p class="text-end fs-6">41 mm</p>
+                  <div class="container col-4">
+                        <h5 class="pt-3 pb-3 fw-bold">Informations boîtier</h5>
+                        <div class="row">
+                              <p class=" fw-bold fs-6 col-6">Diamètre</p>
+                              <p class="text-end fs-6 col-6">41 mm</p>
                         </div>
-                        <div class="">
-                              <p class=" fw-bold fs-6">Épaisseur du boîtier</p>
-                              <p class="text-end fs-6">13 mm</p>
+                        <div class="row">
+                              <p class=" fw-bold fs-6 col-6">Épaisseur du boîtier</p>
+                              <p class="text-end fs-6 col-6">13 mm</p>
                         </div>
-                        <div class="">
-                              <p class=" fw-bold fs-6">Forme du boîtier</p>
-                              <p class="text-end fs-6">Rond</p>
+                        <div class="row">
+                              <p class=" fw-bold fs-6 col-6">Forme du boîtier</p>
+                              <p class="text-end fs-6 col-6">Rond</p>
                         </div>
-                        <div class="">
-                              <p class=" fw-bold fs-6">Couleur du boîtier</p>
-                              <p class="text-end fs-6">Gris</p>
+                        <div class="row">
+                              <p class=" fw-bold fs-6 col-6">Couleur du boîtier</p>
+                              <p class="text-end fs-6 col-6">Gris</p>
                         </div>
-                        <hr>
+                        <hr class="ligneOr">
                         <div>
-                              <h4>Informations bracelet</h4>
-                              <div class="">
-                                    <p class=" fw-bold fs-6">Matière bracelet</p>
-                                    <p class="text-end fs-6">Cuir</p>
+                              <h5 class="pt-3 pb-3">Informations bracelet</h5>
+                              <div class="row">
+                                    <p class=" fw-bold fs-6 col-6">Matière bracelet</p>
+                                    <p class="text-end fs-6 col-6">Cuir</p>
                               </div>
-                              <div class="">
-                                    <p class=" fw-bold fs-6">Couleur du bracelet</p>
-                                    <p class="text-end fs-6">Noir</p>
+                              <div class="row">
+                                    <p class=" fw-bold fs-6 col-6">Couleur du bracelet</p>
+                                    <p class="text-end fs-6 col-6">Noir</p>
                               </div>
-                              <div class="">
-                                    <p class=" fw-bold fs-6">Taille du poignet</p>
-                                    <p class="text-end fs-6">160 mm - 210 mm</p>
+                              <div class="row">
+                                    <p class=" fw-bold fs-6 col-6">Taille du poignet</p>
+                                    <p class="text-end fs-6 col-6">160 mm - 210 mm</p>
                               </div>
                         </div>
                   </div>
             </div>
 
       </section>
-      <section></section>
+      <hr class="container m-5 ligneOr">
+      <section class="container">
+            <div class=" row">
+                  <div class=" col-3 pt-4">
+                        <h4>Egalement disponible dans cette marque</h4>
+                  </div>
+                  <div class="row col-9">
+                        <div class="carte-montre p-3 col-sm-4 col-md-3">
+                              <img src="../assets/img/photos_montres/timex_Q_TW2W70800_1983_E-Line.webp" alt="Montre timex_Q_TW2W70800_1983_E-Line">
+                              <h4 class="text-center pt-3">TIMEX</h4>
+                              <p class="reference text-center">référence(id)</p>
+                              <p class="text-center">désignation 1</p>
+                              <h4 class="text-center fw-bold">50.00€</h4>
+                              <div class="container d-flex justify-content-center">
+                                    <button class="btn ft-btn-produit">Voir les produits</button>
+                              </div>
+                        </div>
+                        <div class="carte-montre p-3 col-sm-4 col-md-3">
+                              <img src="../assets/img/photos_montres/timex_Q_TW2W70800_1983_E-Line.webp" alt="Montre timex_Q_TW2W70800_1983_E-Line">
+                              <h4 class="text-center pt-3">TIMEX</h4>
+                              <p class="reference text-center">référence(id)</p>
+                              <p class="text-center">désignation 1</p>
+                              <h4 class="text-center fw-bold">50.00€</h4>
+                              <div class="container d-flex justify-content-center">
+                                    <button class="btn ft-btn-produit">Voir les produits</button>
+                              </div>
+                        </div>
+                        <div class="carte-montre p-3 col-sm-4 col-md-3">
+                              <img src="../assets/img/photos_montres/timex_Q_TW2W70800_1983_E-Line.webp" alt="Montre timex_Q_TW2W70800_1983_E-Line">
+                              <h4 class="text-center pt-3">TIMEX</h4>
+                              <p class="reference text-center">référence(id)</p>
+                              <p class="text-center">désignation 1</p>
+                              <h4 class="text-center fw-bold">50.00€</h4>
+                              <div class="container d-flex justify-content-center">
+                                    <button class="btn ft-btn-produit">Voir les produits</button>
+                              </div>
+                        </div>
+                        <div class="carte-montre p-3 col-sm-4 col-md-3">
+                              <img src="../assets/img/photos_montres/timex_Q_TW2W70800_1983_E-Line.webp" alt="Montre timex_Q_TW2W70800_1983_E-Line">
+                              <h4 class="text-center pt-3">TIMEX</h4>
+                              <p class="reference text-center">référence(id)</p>
+                              <p class="text-center">désignation 1</p>
+                              <h4 class="text-center fw-bold">50.00€</h4>
+                              <div class="container d-flex justify-content-center">
+                                    <button class="btn ft-btn-produit">Voir les produits</button>
+                              </div>
+                        </div>
+                  </div>
+            </div>
+      </section>
 </main>
 
 
